7420896358142de37a7173ad55a73f776f8deb19
Yuchen Xiong <xiongyuc@bc.edu>
2025-05-20 16:51:27 +0000

    first commit

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
 	title: "Dream Lab Blog",
-	url: "https://makeademic.github.io/dream-lab-blog/",
+	url: "https://YuchenX063.github.io/dream-lab-blog/",
 	language: "en",
 	description: "I am writing about my experiences at Dream Lab 2025",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Yuchen Xiong",
+		email: "xiongyuc@bc.edu",
+		url: "https://template.com/about-me/"
 	}
 }
